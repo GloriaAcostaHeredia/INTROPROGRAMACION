78f4b2a197905aca9705a274ecb9af0fbbcf50be
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-02-11 14:33:01 -0600

    Update 11.02.2020 segunda parte la derrota de golith.php

--- 11.02.2020 segunda parte la derrota de golith.php
+++ 11.02.2020 segunda parte la derrota de golith.php
@@ -2,20 +2,58 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> C A M B I O   Y  F U E R A </title>
+	<title> C O L O R S </title>
 </head>
 <BODY>
 	<H2>
-	  Ingresando una cantidad, que sea menor o igual a 100 dolares realice el tipo de cambio a pesos
+		Ingresando el nombre deun color imprima color encontrado si el nombre de color es igual al almacenado dentro del algortimo. Minimo de almacenamiento 10
 	</H2>
 	<?php
-	$Cant=99;
-	if ($Cant<=100) {
-		$T=$Cant/18.66;
-		echo "Su cambio es de $T";
+	$Question="Turquesa"
+	$Color1="Azul";
+	$Color2="Rojo";
+	$Color3="Verde";
+	$Color4="Morado";
+	$Color5="Rosa";
+	$Color6="Amarrillo";
+	$Color7="Blanco";
+	$Color8="Gris";
+	$Color9="Naranja";
+	$Color10="Negro";
+	echo "EL color $Question esta...";
+	if ($Question=$Color1) {
+		echo "Encontrado en el catalogo";
 	}
-	if ($Cant>100) {
-		echo"Su cambio es de $Cant";
+	if ($Question=$Color2) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color3) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color4) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color5) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color6) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color7) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color8) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color9) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color10) {
+		echo "Encontrado en el catalogo";
+	}
+
+	if ($Question<>$Color1,$Color2,$Color3,$Color4,$Color5,$Color6,$Color7,$Color8,$Color9 o $Color10) {
+		echo "No encontrado en el catalogo";
 	}
 	?>
 </BODY>
