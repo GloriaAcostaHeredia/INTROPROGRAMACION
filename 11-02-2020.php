ed31235d145ce7a7af4b4adc1fc0103746f55ff6
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-02-11 14:51:54 -0600

    Add files via upload

---
+++ 11-02-2020.php
@@ -0,0 +1,60 @@
+11/2/2020
+<!DOCTYPE html>
+<html>
+<head>
+	<title> C O L O R S </title>
+</head>
+<BODY>
+	<H2>
+		Ingresando el nombre deun color imprima color encontrado si el nombre de color es igual al almacenado dentro del algortimo. Minimo de almacenamiento 10
+	</H2>
+	<?php
+	$Question="Turquesa"
+	$Color1="Azul";
+	$Color2="Rojo";
+	$Color3="Verde";
+	$Color4="Morado";
+	$Color5="Rosa";
+	$Color6="Amarrillo";
+	$Color7="Blanco";
+	$Color8="Gris";
+	$Color9="Naranja";
+	$Color10="Negro";
+	echo "EL color $Question esta...";
+	if ($Question=$Color1) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color2) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color3) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color4) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color5) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color6) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color7) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color8) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color9) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question=$Color10) {
+		echo "Encontrado en el catalogo";
+	}
+
+	if ($Question<>$Color1,$Color2,$Color3,$Color4,$Color5,$Color6,$Color7,$Color8,$Color9 o $Color10) {
+		echo "No encontrado en el catalogo";
+	}
+	?>
+</BODY>
+</html>
