f7f9acb31eb792484e776640adeb28e957b2931c
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-02-11 14:16:39 -0600

    Add files via upload

---
+++ 11-02-2020.php
@@ -0,0 +1,23 @@
+11/2/2020
+<!DOCTYPE html>
+<html>
+<head>
+	<title> C O L O R S </title>
+</head>
+<BODY>
+	<H2>
+		Ingresando el nombre deun color imprima color encontrado si el nombre de color es igual al almacenado dentro del algortimo. Minimo de almacenamiento 10
+	</H2>
+	<?php
+	$Question=$Turquesa
+	$Colores=$Azul,$Rojo,$Verde,$Morado,$Rosa,$Amarrillo,$Blanco,$Gris,$Naranja,$Negro
+	echo "EL color $Question esta...";
+	if ($Question=$Colores) {
+		echo "Encontrado en el catalogo";
+	}
+	if ($Question<>$Colores) {
+		echo "No encontrado en el catalogo";
+	}
+	?>
+</BODY>
+</html>
