91d2e4f8a57ca03b8114cd8d6ef3b17740249f0c
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-02-27 15:35:01 -0600

    Update Comprobacioon.php

--- examen/Comprobacioon.php
+++ examen/Comprobacioon.php
@@ -6,7 +6,7 @@
 <h1>Resultados</h1>
 	<?php
     echo $_POST['Multi'];echo " "; echo "X";echo " "; echo $_POST['a']; 
-
+     echo"<br>"
     echo $_POST['a']*$_POST['Multi'];
 ?>
 <p>Volver a consultar: <a href="index.php">VOLVER</a>
