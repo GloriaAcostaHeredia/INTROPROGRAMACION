a5ecb58f00273b3d52d59b3d809979c909090120
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-02-27 15:32:53 -0600

    Add files via upload

---
+++ examen/Comprobacioon.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultados</title>
+</head>
+<h1>Resultados</h1>
+	<?php
+    echo $_POST['Multi'];echo " "; echo "X";echo " "; echo $_POST['a']; 
+
+    echo $_POST['a']*$_POST['Multi'];
+?>
+<p>Volver a consultar: <a href="index.php">VOLVER</a>
---
+++ examen/indexx.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MI MUERTE</title>
+</head>
+<form action="Comprobacioon.php" method="post"/> 
+/*PRIMER CON LIMITE */
+	<h3>Seleccione numero</h3>
+	<select name="a">
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+	<option value="5">5</option>
+	<option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+	</select>
+	<br>
+	<br>
+	<p>
+  /*SEGUNDO A CALCULAR */
+<input type="number" name="Multi">
+
+<input type="submit" value="Generar" name="enviar">
+</p>
+</form>
+
+
+</body>
+</html>
