f833988996989779415c66666dba3d4e1bdff834
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-02-27 15:30:32 -0600

    Add files via upload

---
+++ examen/Comprobacioon.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultados</title>
+</head>
+<h1>Resultados</h1>
+	<?php
+    echo $_POST['Multi']. echo "X". echo $_POST['a']; 
+    echo $_POST['a']*$_POST['Multi'];
+?>
+<p>Volver a consultar: <a href="indexx.php">VOLVER</a>
