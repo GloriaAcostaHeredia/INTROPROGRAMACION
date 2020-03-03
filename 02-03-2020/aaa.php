799c07e4be55e005b19d9d8760f30dd9c480bda4
GloriaAcostaHeredia <60714360+GloriaAcostaHeredia@users.noreply.github.com>
2020-03-02 22:24:58 -0600

    Add files via upload

---
+++ 02-03-2020/aaa.php
@@ -0,0 +1,19 @@
+<?php
+/*1.almacena en un aray los 10 primeros numeros pares.Imprjmierlos en una sola linea separadors por una coma*/
+/*Se llena el array*/
+$num[0]="2";
+$num[1]="4";
+$num[2]="6";
+$num[3]="8";
+$num[4]="10";
+$num[5]="12";
+$num[6]="14";
+$num[7]="16";
+$num[8]="18";
+$num[9]="20";
+echo"<br>";
+echo"LOS 10 PRIMEROS NUEMROS PRIMOS SON:";
+echo"<br>";
+/*Se imprime los numeros separados por una coma */
+echo$num[0];echo","; echo$num[1];echo",";echo$num[2];echo","; echo$num[3];echo",";echo$num[4];echo","; echo$num[5];echo",";echo$num[6];echo","; echo$num[7];echo",";echo$num[8];echo","; echo$num[9];
+?>
---
+++ 02-03-2020/array.php
@@ -0,0 +1,35 @@
+
+<?php
+$nomb[0]="Aldo";
+$nomb[1]="Pedro";
+$nomb[2]="Jose";
+$nomb[3]="Alberto";
+$nomb[4]="Toño";
+$nomb[5]="Valetina";
+$nomb[6]="Juan";
+$nomb[7]="Daniela";
+$nomb[8]="Cordelia";
+$nomb[9]="Sarah";
+echo"<br>";
+echo$nomb[5];
+
+$ca[0]="Derecho";
+$ca[1]="Arquitectura";
+$ca[2]="ITI";
+$ca[3]="Medicina";
+$ca[4]="Ingenieria Civil";
+$ca[5]="Veterinaria";
+$ca[6]="Aeroespacial";
+$ca[7]="Artes y humanidades";
+$ca[8]="Ninja";
+$ca[9]="Botanica";
+echo"<br>";
+echo$ca[8];
+echo"<br>";
+if ($ca=="ITI") {
+	echo "ES DE ITI";
+}else{
+	echo"NO ES ITI";
+}
+?>
+
